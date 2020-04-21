9490512d07ef08672d8394e9679ead55317cb3f9
unknown <809016056@qq.com>
2020-04-21 15:11:50 +0800

    构建项目、配置路由、elementui

---
+++ vue-admin/src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    这是登录页
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+  background-color:gray;
+  // vh 单位是基于可视区窗口的占比设置的，100vh是满屏，50vh是半屏
+  height: 100vh;
+}
+</style>
